b2d12f1994f5abb843cc68ca83b1a6cca0b01998
raminabbassi <rai21008@student.mdu.se>
2024-12-13 13:03:13 +0100

    Xunit
    Fixed Unitester

---
+++ StaffingSolutionTest/JobTests.cs
@@ -0,0 +1,53 @@
+﻿using StaffingSolution.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace StaffingSolution.Tests
+{
+    public class JobTests
+    {
+        [Fact]
+        public void Job_DefaultConstructor_ShouldSetPropertiesToNull()
+        {
+            // Arrange & Act
+            var job = new Job();
+
+            // Assert
+            Assert.Null(job.Title);
+            Assert.Null(job.Location);
+            Assert.Null(job.Description);
+            Assert.Null(job.ImageUrl);
+            Assert.Null(job.Responsibilities);
+            Assert.Null(job.Requirements);
+
+        }
+
+        [Fact]
+        public void Job_SetProperties_ShouldStoreValues()
+        {
+            // Arrange
+            var job = new Job
+            {
+                Title = "Software Developer",
+                Location = "Stockholm",
+                Description = "Develop and maintain software solutions.",
+                ImageUrl = "https://example.com/image.jpg",
+                Responsibilities="Packa och organisera varor",
+                Requirements=" Minst 1 års erfarenhet av lagerarbete"
+            };
+
+            // Assert
+            Assert.Equal("Software Developer", job.Title);
+            Assert.Equal("Stockholm", job.Location);
+            Assert.Equal("Develop and maintain software solutions.", job.Description);
+            Assert.Equal("https://example.com/image.jpg", job.ImageUrl);
+            Assert.Equal("Packa och organisera varor", job.Responsibilities);
+            Assert.Equal(" Minst 1 års erfarenhet av lagerarbete", job.Requirements);
+
+        }
+    }
+}
